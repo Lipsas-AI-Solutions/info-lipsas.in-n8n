6d7f5708250953c3f7ed022072b6c6d33889b40b
Lipsas-AI-Solutions <info@lipsas.in>
2025-10-18 15:53:59 +0530

    Update text_extractor_api.py

--- text_extractor_api.py
+++ text_extractor_api.py
@@ -60,4 +60,4 @@ def extract_text():
         return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5678)
+    app.run(host='0.0.0.0', port=5000)  # Changed from 5678 to 5000
